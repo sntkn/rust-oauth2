fbd67bbf5b016a0e005433a2ab706dd1af2aa107
sntkn <mail.tsurumi@gmail.com>
2024-03-14 21:35:23 +0900

    Hello world

--- oauth2/src/main.rs
+++ oauth2/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Json, Router};
+use serde::Serialize;
+
+#[tokio::main]
+async fn main() {
+    let router = Router::new().route("/", get(hello_world));
+    let listner = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listner, router).await.unwrap();
+}
+
+async fn hello_world() -> Json<HelloWorld> {
+    let hello = HelloWorld {
+        text: "Hello World".to_string(),
+    };
+    Json(hello)
+}
+
+#[derive(Serialize)]
+struct HelloWorld {
+    text: String,
 }
