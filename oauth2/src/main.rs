f3978aa3d1f988bc4b3b8c7fdf75e81ae6f94d38
sntkn <mail.tsurumi@gmail.com>
2024-03-18 21:30:29 +0900

    Add html templates

--- oauth2/src/main.rs
+++ oauth2/src/main.rs
@@ -1,9 +1,18 @@
-use axum::{routing::get, Json, Router};
+use askama::Template;
+use axum::{
+    extract,
+    http::StatusCode,
+    response::{Html, IntoResponse, Response},
+    routing::get,
+    Json, Router,
+};
 use serde::Serialize;
 
 #[tokio::main]
 async fn main() {
-    let router = Router::new().route("/", get(hello_world));
+    let router = Router::new()
+        .route("/", get(hello_world))
+        .route("/greet/:name", get(greet));
     let listner = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listner, router).await.unwrap();
 }
@@ -18,4 +27,33 @@ async fn hello_world() -> Json<HelloWorld> {
 #[derive(Serialize)]
 struct HelloWorld {
     text: String,
+}
+
+async fn greet(extract::Path(name): extract::Path<String>) -> impl IntoResponse {
+    let template = HelloTemplate { name };
+    HtmlTemplate(template)
+}
+
+#[derive(Template)]
+#[template(path = "hello.html")]
+struct HelloTemplate {
+    name: String,
+}
+
+struct HtmlTemplate<T>(T);
+
+impl<T> IntoResponse for HtmlTemplate<T>
+where
+    T: Template,
+{
+    fn into_response(self) -> Response {
+        match self.0.render() {
+            Ok(html) => Html(html).into_response(),
+            Err(err) => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Failed to render template. Error: {}", err),
+            )
+                .into_response(),
+        }
+    }
 }
