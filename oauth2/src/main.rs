bc076b990603d0a310c457880e9c13a54ff72c9d
sntkn <mail.tsurumi@gmail.com>
2024-03-24 17:52:02 +0900

    validation in the middle

--- oauth2/src/main.rs
+++ oauth2/src/main.rs
@@ -12,12 +12,19 @@ use axum::{
     Json, Router,
 };
 use axum_valid::Valid;
+use regex::Regex;
 use sea_orm::*;
 use serde::{Deserialize, Serialize};
-use validator::Validate;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
+use uuid::Uuid;
+use validator::{Validate, ValidationError};
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::registry()
+        .with(EnvFilter::try_from_default_env().unwrap_or_else(|_| "debug".into()))
+        .with(tracing_subscriber::fmt::layer())
+        .init();
     let db_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env file");
     let conn: DatabaseConnection = Database::connect(db_url).await.unwrap();
 
@@ -56,13 +63,41 @@ struct HelloWorld {
 #[derive(Debug, Deserialize, Validate)]
 pub struct AuthorizeInput {
     #[validate(length(min = 1, message = "Paramater 'response_type' can not be empty"))]
+    #[validate(custom(function = "match_code"))]
     pub response_type: String,
     #[validate(length(min = 1, message = "Paramater 'state' can not be empty"))]
     pub state: String,
     #[validate(length(min = 1, message = "Paramater 'client_id' can not be empty"))]
+    #[validate(custom(function = "uuid"))]
     pub client_id: String,
     #[validate(length(min = 1, message = "Paramater 'redirect_uri' can not be empty"))]
+    #[validate(url)]
     pub redirect_uri: String,
+}
+
+pub fn match_code(v: &str) -> Result<(), ValidationError> {
+    if v == "code" {
+        Ok(())
+    } else {
+        Err(ValidationError::new(
+            "Paramater 'response_type' should be 'code'",
+        ))
+    }
+}
+
+pub fn uuid(id: &str) -> Result<(), ValidationError> {
+    // Define the regular expression pattern for UUIDv4
+    let pattern =
+        Regex::new(r"^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$")
+            .expect("Failed to compile UUID regex pattern");
+
+    if !pattern.is_match(id) {
+        let mut error = ValidationError::new("Invalid UUID format");
+        error.add_param(std::borrow::Cow::Borrowed("pattern"), &pattern.to_string());
+        return Err(error);
+    }
+
+    Ok(())
 }
 
 //#[async_trait]
@@ -93,7 +128,15 @@ async fn greet(extract::Path(name): extract::Path<String>) -> impl IntoResponse 
     HtmlTemplate(template)
 }
 
-async fn authorize(Valid(Query(input)): Valid<Query<AuthorizeInput>>) -> impl IntoResponse {
+async fn authorize(
+    Valid(Query(input)): Valid<Query<AuthorizeInput>>,
+    state: State<AppState>,
+) -> impl IntoResponse {
+    let parsed_uuid = Uuid::parse_str(&input.client_id).unwrap();
+    let client = OAuth2ClientEntity::find_by_id(parsed_uuid)
+        .one(&state.conn)
+        .await; // .unwrap();
+
     let template = AuthorizeTemplate { state: input.state };
 
     // validattion
