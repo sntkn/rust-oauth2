84032714877075a60f343fef21c577f3a9d90cc9
sntkn <mail.tsurumi@gmail.com>
2024-03-30 09:23:28 +0900

    Forced implementation of process to store sessions in cookies

--- oauth2/src/main.rs
+++ oauth2/src/main.rs
@@ -8,12 +8,15 @@ use async_redis_session::RedisSessionStore;
 use async_session::{Session, SessionStore};
 use axum::{
     debug_handler,
-    extract::{self, Query, State},
+    extract::{self, FromRequestParts, Query, Request, State},
     http::StatusCode,
+    middleware::{self, Next},
     response::{Html, IntoResponse, Redirect, Response},
     routing::{get, post},
     Form, Json, Router,
 };
+use axum_extra::extract::cookie::{self, Cookie as EntityCookie, CookieJar};
+use axum_extra::{headers::Cookie, TypedHeader};
 use axum_valid::Valid;
 use regex::Regex;
 use sea_orm::*;
@@ -32,26 +35,79 @@ async fn main() {
     let conn: DatabaseConnection = Database::connect(db_url).await.unwrap();
 
     let store = RedisSessionStore::new("redis://localhost:6379/").unwrap();
-    let session = Session::new();
-    let cookie_value = store.store_session(session).await.unwrap().unwrap();
-    let session = store.load_session(cookie_value).await.unwrap().unwrap();
+    //let session = Session::new();
+    //let cookie_value = store.store_session(session).await.unwrap().unwrap();
+    //let session = store.load_session(cookie_value).await.unwrap().unwrap();
+    //let cookie = store.store_session(session).await.unwrap().unwrap();
+    //let t = SessionToken(cookie);
 
-    let state = AppState { conn, session };
+    let state = AppState { conn, store };
 
     let router = Router::new()
         .route("/", get(hello_world))
         .route("/greet/:name", get(greet))
         .route("/authorize", get(authorize)) // http://localhost:3000/authorize?response_type=code&state=3&client_id=550e8400-e29b-41d4-a716-446655440000&redirect_uri=http%3A%2F%2Flocalhost%3A8000%2Fcallback
         .route("/authorization", post(authorization))
+        //.layer(middleware::from_fn_with_state(
+        //    state.clone(),
+        //    print_request_response,
+        //))
         .with_state(state);
     let listner = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listner, router).await.unwrap();
 }
 
+async fn print_request_response(
+    State(state): State<AppState>,
+    req: Request,
+    next: Next,
+) -> Result<(CookieJar, impl IntoResponse), (StatusCode, String)> {
+    println!("================== middleware =================");
+    let session = Session::new();
+    let cookie_value = state.store.store_session(session).await.unwrap().unwrap();
+
+    let jar = CookieJar::from_headers(req.headers());
+
+    let cc = EntityCookie::new("session_id", cookie_value);
+
+    let cookie = jar.get("session_id").unwrap_or(&cc);
+
+    let j = jar.clone().add(cookie.clone());
+
+    Ok((j, next.run(req).await))
+}
+
+//pub async fn set_and_store_cookie_if_absent<B>(
+//    State(state): State<AppState>,
+//    cookies: Cookies,
+//    request: Request<B>,
+//    next: Next<B>,
+//) -> Response {
+//    if cookies.get(SESSION_COOKIE).is_none() {
+//        // handle, write to State, etc.
+//    }
+//
+//    next.run(request).await
+//}
+
+async fn load_session(store: &RedisSessionStore, req: Request) -> (Session, CookieJar) {
+    let session = Session::new();
+    let cookie_value = store.store_session(session).await.unwrap().unwrap();
+    let jar = CookieJar::from_headers(req.headers());
+    let cc = EntityCookie::new("session_id", cookie_value);
+    let cookie = jar.get("session_id").unwrap_or(&cc);
+    let jc = jar.clone().add(cookie.clone());
+    let cv = cookie.value();
+    println!("cookie: {:?}", cv);
+    let session = store.load_session(cv.to_string()).await.unwrap().unwrap();
+    (session, jc)
+}
+
 #[derive(Clone)]
 struct AppState {
     conn: DatabaseConnection,
-    session: Session,
+    //session: SessionToken,
+    store: RedisSessionStore,
 }
 
 async fn hello_world(state: State<AppState>) -> Result<impl IntoResponse, StatusCode> {
@@ -72,20 +128,31 @@ struct HelloWorld {
 
 #[derive(Debug, Deserialize, Validate)]
 struct AuthorizeInput {
+    #[serde(default)]
     #[validate(length(min = 1, message = "Paramater 'response_type' can not be empty"))]
     #[validate(custom(function = "match_code"))]
     response_type: String,
 
+    #[serde(default)]
     #[validate(length(min = 1, message = "Paramater 'state' can not be empty"))]
     state: String,
 
+    #[serde(default)]
     #[validate(length(min = 1, message = "Paramater 'client_id' can not be empty"))]
     #[validate(custom(function = "uuid"))]
     client_id: String,
 
+    #[serde(default)]
     #[validate(length(min = 1, message = "Paramater 'redirect_uri' can not be empty"))]
     #[validate(url)]
     redirect_uri: String,
+}
+#[derive(Debug, Deserialize)]
+struct AuthorizeValue {
+    response_type: Option<String>,
+    state: Option<String>,
+    client_id: Option<String>,
+    redirect_uri: Option<String>,
 }
 
 #[derive(Debug, Deserialize, Validate)]
@@ -103,6 +170,8 @@ struct AuthorizationInput {
 struct AuthorizeJson {
     client_id: String,
     redirect_uri: String,
+    state: String,
+    response_type: String,
 }
 
 fn validate_password(password: &str) -> Result<(), ValidationError> {
@@ -178,9 +247,36 @@ async fn greet(extract::Path(name): extract::Path<String>) -> impl IntoResponse 
 }
 
 async fn authorize(
-    mut state: State<AppState>,
-    Valid(Query(input)): Valid<Query<AuthorizeInput>>,
-) -> Result<impl IntoResponse, StatusCode> {
+    state: State<AppState>,
+    Query(mut input): Query<AuthorizeInput>,
+    req: Request,
+) -> Result<(CookieJar, impl IntoResponse), StatusCode> {
+    let (mut session, jar) = load_session(&state.store, req).await;
+    // セッションをまず取得して、さらにリクエストパラメータがあったら上書きする
+    let sess_val = session
+        .get::<String>("session_id")
+        .unwrap_or("{}".to_string());
+    let auth_val: AuthorizeValue = serde_json::from_str(&sess_val).unwrap();
+
+    if input.response_type.is_empty() && auth_val.response_type.is_some() {
+        input.response_type = auth_val.response_type.unwrap();
+    }
+    if input.state.is_empty() && auth_val.state.is_some() {
+        input.state = auth_val.state.unwrap();
+    }
+    if input.client_id.is_empty() && auth_val.client_id.is_some() {
+        input.client_id = auth_val.client_id.unwrap();
+    }
+    if input.redirect_uri.is_empty() && auth_val.redirect_uri.is_some() {
+        input.redirect_uri = auth_val.redirect_uri.unwrap();
+    }
+    println!("{:?}", input);
+
+    if let Err(errors) = input.validate() {
+        println!("{:#?}", errors);
+        return Err(StatusCode::UNAUTHORIZED);
+    }
+
     let parsed_uuid = Uuid::parse_str(&input.client_id).unwrap();
     // client_id が そんざいすること
     let client = OAuth2ClientEntity::find_by_id(parsed_uuid)
@@ -190,28 +286,32 @@ async fn authorize(
         .ok_or(StatusCode::FORBIDDEN)?;
 
     // redirect_uri が一致すること
-    if input.redirect_uri != client.redirect_uris {
+    let redirect_uri = input.redirect_uri;
+    if redirect_uri != client.redirect_uris {
         return Err(StatusCode::FORBIDDEN);
     }
 
     let template = AuthorizeTemplate {
-        state: input.state,
+        state: input.state.clone(),
         client_id: client.id.to_string(),
     };
 
     let json = AuthorizeJson {
-        client_id: input.client_id.to_string(),
-        redirect_uri: input.redirect_uri.to_string(),
+        client_id: client.id.to_string(),
+        redirect_uri,
+        state: input.state,
+        response_type: input.response_type,
     };
 
     let val = serde_json::to_string(&json).unwrap();
 
     // リクエストパラメータをセッションに保存する
-    state.session.insert("key", val).unwrap();
-    let val = state.session.get::<String>("key").unwrap();
-    println!("session is {}", &val);
+    session.insert("session_id", val).unwrap();
+    let val = session.get::<String>("session_id").unwrap();
+    println!("session is {:#?}", &val);
+    state.store.store_session(session).await;
     // ログインフォームを表示する
-    Ok(HtmlTemplate(template))
+    Ok((jar, HtmlTemplate(template)))
 }
 
 // ログインチェック
@@ -226,7 +326,8 @@ async fn authorization(
     Form(input): Form<AuthorizationInput>,
 ) -> impl IntoResponse {
     println!("{:#?}", input);
-    let val = state.session.get::<String>("key").unwrap();
+    //let val = session.get::<String>("key").unwrap();
+    //println!("session is {}", &val);
     if let Err(errors) = input.validate() {
         println!("{:#?}", errors);
         Redirect::to("/autorize")
