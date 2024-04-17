57a8c631dd66408cc1dc9bc13f2fceb4c5d357bf
sntkn <mail.tsurumi@gmail.com>
2024-04-18 08:31:14 +0900

    add header logout link

--- front/app/header.tsx
+++ front/app/header.tsx
@@ -1,6 +1,9 @@
 import Link from 'next/link'
+import { User } from '../entity'
+import { session } from '../lib/session'
 
-const Header = () => {
+const Header = async () => {
+  const user: User = await session().get('user')
   return (
     <header className="bg-blue-500 p-4">
       <div className="container mx-auto">
@@ -10,12 +13,25 @@ const Header = () => {
           </Link>
           <ul className="flex space-x-4">
             <li>
-              <Link
-                href="http://localhost:3000/authorize?response_type=code&client_id=550e8400-e29b-41d4-a716-446655440000&scope=read&redirect_uri=http%3A%2F%2Flocalhost%3A8000%2Fcallback&state=ok"
-                className="text-white hover:underline"
-              >
-                Login
-              </Link>
+              {!user && (
+                <Link
+                  href="http://localhost:3000/authorize?response_type=code&client_id=550e8400-e29b-41d4-a716-446655440000&scope=read&redirect_uri=http%3A%2F%2Flocalhost%3A8000%2Fcallback&state=ok"
+                  className="text-white hover:underline"
+                >
+                  Login
+                </Link>
+              )}
+              {user && (
+                <>
+                  <span>ようこそ {user.name} さん</span>
+                  <Link
+                    href="http://localhost/"
+                    className="text-white hover:underline"
+                  >
+                    ログアウト
+                  </Link>
+                </>
+              )}
             </li>
             <li>
               <Link href="/contact" className="text-white hover:underline">
