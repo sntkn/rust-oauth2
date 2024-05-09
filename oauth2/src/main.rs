1493a8e667dea860a61424fcfb21c5c927cc27f1
sntkn <mail.tsurumi@gmail.com>
2024-05-09 20:59:55 +0900

    remove uncomment

--- oauth2/src/main.rs
+++ oauth2/src/main.rs
@@ -346,29 +346,6 @@ fn uuid(id: &str) -> Result<(), ValidationError> {
     Ok(())
 }
 
-//#[async_trait]
-//impl<T, S> FromRequest<S> for ValidatedForm<T>
-//where
-//    T: DeserializeOwned + Validate,
-//    S: Send + Sync,
-//{
-//    type Rejection = (StatusCode, String);//
-
-//    async fn from_request(req: Request, state: &S) -> Result<Self, Self::Rejection> {
-//        let Json(value) = Json::<T>::from_request(req, state)
-//            .await
-//            .map_err(|rejection| {
-//                let message = format!("Json parse error: [{}]", rejection);
-//                (StatusCode::BAD_REQUEST, message)
-//            })?;
-//        value.validate().map_err(|rejection| {
-//            let message = format!("Validation error: [{}]", rejection).replace('\n', ", ");
-//            (StatusCode::BAD_REQUEST, message)
-//        })?;
-//        Ok(ValidatedForm(value))
-//    }
-//}
-
 async fn authorize(
     State(state): State<AppState>,
     Extension(session): Extension<Session>,
