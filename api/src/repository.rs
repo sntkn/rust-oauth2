6e44c56e199799a2fc96c143526c1f92f571c3e1
sntkn <mail.tsurumi@gmail.com>
2024-05-23 21:50:45 +0900

    add article create, update ,find

--- api/src/repository.rs
+++ api/src/repository.rs
@@ -1,13 +1,25 @@
 use crate::entity::{
     articles,
-    users::{self, Column},
+    users::{self},
 };
+use chrono::Local;
 use sea_orm::*;
 use uuid::Uuid;
 
 pub struct EditUserParams {
     pub name: Option<String>,
     pub email: Option<String>,
+}
+
+pub struct CreateArticleParams {
+    pub author_id: Uuid,
+    pub title: String,
+    pub content: String,
+}
+
+pub struct UpdateArticleParams {
+    pub title: Option<String>,
+    pub content: Option<String>,
 }
 
 #[derive(Clone)]
@@ -46,5 +58,58 @@ impl Repository {
             .order_by_desc(articles::Column::CreatedAt)
             .all(&self.db)
             .await
+    }
+
+    pub async fn find_article(&self, id: Uuid) -> Result<Option<articles::Model>, DbErr> {
+        articles::Entity::find_by_id(id).one(&self.db).await
+    }
+
+    pub async fn find_articles_by_author(
+        &self,
+        author_id: Uuid,
+    ) -> Result<Vec<articles::Model>, DbErr> {
+        articles::Entity::find()
+            .filter(articles::Column::AuthorId.eq(author_id))
+            .order_by_desc(articles::Column::CreatedAt)
+            .all(&self.db)
+            .await
+    }
+
+    pub async fn create_article(
+        &self,
+        payload: CreateArticleParams,
+    ) -> Result<articles::Model, DbErr> {
+        let id = Uuid::new_v4();
+        let article = articles::ActiveModel {
+            id: ActiveValue::set(id),
+            author_id: ActiveValue::set(payload.author_id),
+            title: ActiveValue::set(payload.title),
+            content: ActiveValue::set(payload.content),
+            created_at: ActiveValue::set(Some(Local::now().naive_local())),
+            updated_at: ActiveValue::set(Some(Local::now().naive_local())),
+        };
+
+        article.insert(&self.db).await
+    }
+
+    pub async fn update_article(
+        &self,
+        id: Uuid,
+        payload: UpdateArticleParams,
+    ) -> Result<articles::Model, DbErr> {
+        let mut article = self
+            .find_article(id)
+            .await?
+            .ok_or_else(|| DbErr::Custom("Article not found.".to_owned()))?
+            .into_active_model();
+
+        if let Some(title) = payload.title {
+            article.title = Set(title);
+        }
+        if let Some(content) = payload.content {
+            article.content = Set(content)
+        }
+        article.updated_at = Set(Some(Local::now().naive_local()));
+        article.update(&self.db).await
     }
 }
