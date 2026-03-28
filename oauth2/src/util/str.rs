77ac4ee39a3df0d3510c860fc81ec833cfb797e3
sntkn <mail.tsurumi@gmail.com>
2026-03-28 09:33:39 +0900

    update cargo

--- oauth2/src/util/str.rs
+++ oauth2/src/util/str.rs
@@ -1,8 +1,8 @@
-use rand::{distributions::Alphanumeric, Rng};
+use rand::{distr::Alphanumeric, RngExt};
 
 pub fn generate_random_string(len: usize) -> String {
-    let random_bytes: Vec<u8> = rand::thread_rng()
-        .sample_iter(&Alphanumeric)
+    let random_bytes: Vec<u8> = rand::rng()
+        .sample_iter(Alphanumeric)
         .take(len)
         .collect();
     String::from_utf8(random_bytes).unwrap()
